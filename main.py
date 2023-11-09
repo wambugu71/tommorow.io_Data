22e57a934b2d591391fcbace644b2bc3e58b57b0
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-11-10 00:12:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#main api file
+import  requests
+import  json
+from datetime  import  datetime
+current_datetime = datetime.now()
+formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")  # Format: YYYY-MM-DD_HH-MM-SS
+file_name = f"Data_{formatted_datetime}.json"
+url = "https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=Uuww83qb9dv3Ehj9L3ugaO1PF4DD3D4Z"
+response = requests.get(url)
+with open(file_name, "wb") as json_file:
+    json_file.write(response.content)
+print(f"Data has been written to {file_name}")
