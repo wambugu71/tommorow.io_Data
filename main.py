f090cc156aca6e770099f0d6ded6ec65031c66ce
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-11-10 16:13:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+import pyrebase
 import  requests
 import  json
 from datetime  import  datetime
@@ -6,6 +8,31 @@ formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")  # Format: Y
 file_name = f"Data_{formatted_datetime}.json"
 url = "https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=Uuww83qb9dv3Ehj9L3ugaO1PF4DD3D4Z"
 response = requests.get(url)
-with open(file_name, "wb") as json_file:
-    json_file.write(response.content)
+file   =  open(file_name, "wb")
+file.write(response.content)
 print(f"Data has been written to {file_name}")
+config = {
+  "apiKey": "AIzaSyDmVHSWHzIJGL-tMzknE90TNGjTWfm9bcs",
+  "authDomain": "weatherdata-51a1f.firebaseapp.com",
+  "databaseURL": "https://weatherdata-51a1f-default-rtdb.firebaseio.com",
+  "storageBucket": "weatherdata-51a1f.appspot.com",
+  "serviceAccount": "weatherdata-51a1f-firebase-adminsdk-n0c0k-6b36a3a194.json"
+}
+
+
+
+firebase = pyrebase.initialize_app(config)
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+
+path_on_cloud = "wambugu.weather_data.csv"
+
+path_local =file_name
+
+
+# Upload
+storage.child(file_name).put(path_local)
+
+# Download
+#storage.child(path_on_cloud).download("<file_downloaded>")
