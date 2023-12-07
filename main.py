6333b83aed5691c6c77a931584c273d92b46460a
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-12-07 16:38:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pyrebase
 import  requests
 import  json
+import  pandas as  pd
 from datetime  import  datetime
 current_datetime = datetime.now()
 formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")  # Format: YYYY-MM-DD_HH-MM-SS
@@ -10,6 +11,19 @@ url = "https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&api
 response = requests.get(url)
 file   =  open(file_name, "wb")
 file.write(response.content)
+# features added.
+responseData = response.json()
+data = responseData["timelines"]["minutely"]
+df = pd.DataFrame(data)
+data_values = []
+for i in range(list(df["values"].shape)[0]):
+  data_values.append(df["values"][i].values())
+data = pd.DataFrame(data_values, columns = df["values"][0].keys())
+data["time"] = df["time"]
+path2  =  f"weather{formatted_datetime}.csv"
+
+data.to_csv(path2)
+
 print(f"Data has been written to {file_name}")
 config = {
   "apiKey": "AIzaSyDmVHSWHzIJGL-tMzknE90TNGjTWfm9bcs",
@@ -30,7 +44,7 @@ path_local =file_name
 
 
 # Upload
-storage.child(file_name).put(path_local)
+storage.child(path2).put(path2)
 
 # Download
 #storage.child(path_on_cloud).download("<file_downloaded>")
