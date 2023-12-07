44c214360136fe94c7d30fc9a59320640a8289b0
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-12-07 20:59:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime  import  datetime
 current_datetime = datetime.now()
 formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")  # Format: YYYY-MM-DD_HH-MM-SS
 file_name = f"Data_{formatted_datetime}.json"
-url = "https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=Uuww83qb9dv3Ehj9L3ugaO1PF4DD3D4Z"
+url = "https://api.tomorrow.io/v4/weather/forecast?location=-0.416667,36.950000&apikey=Uuww83qb9dv3Ehj9L3ugaO1PF4DD3D4Z"
 response = requests.get(url)
 file   =  open(file_name, "wb")
 file.write(response.content)
